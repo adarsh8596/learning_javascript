0dbc658a7b26d40671198c6d0467a88808abb39a
adarsh8596 <adarsh8596@gmail.com>
2025-08-06 23:12:52 +0530

    Day1

--- Day1.js
+++ Day1.js
@@ -1,3 +1,4 @@
+const prompt=require("prompt-sync")({sigint:true}); 
 // function to reverese the number:
 let num=0;
 
@@ -10,3 +11,23 @@ function reverse_number(n){
 }
 
 reverse_number(123);
+
+// Program to add, subtract, multiply,divide two number
+
+let a = 10;
+let b = 20;
+
+console.log("sum of a & b is:",(a+b));
+console.log("subtraction of a & b is:",(a-b));
+console.log("Multiplication of a & b is:",(a*b));
+console.log("Division of a & b is:",(a/b));
+
+// to check if both numbers are equal:
+console.log("is a equal to b:",a===b);
+console.log("is a not equal to b:",a!==0);
+
+// Mini Project:
+
+let userName=prompt("Enter name");
+let age = prompt("Enter age");
+console.log("Hello",userName,"next year you will be",(age+1),"year old");
